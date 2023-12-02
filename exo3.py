44b03bc2031199c352d45c7c31dad1649ef70832
farouck98 <latoundjifarouck@gmail.com>
2023-12-02 12:38:53 +0100

    Exercice 3

--- exo3.py
+++ exo3.py
@@ -1,3 +1,6 @@
 a = float(input("Entrez un nombre a: "))
 b = float(input("Entrez un second nombre b: "))
-print(f"le maximum entre ses deux nombres est {max(a, b)}")
+if a>b:
+    max_number = a
+max_number = b
+print(f"le maximum entre ses deux nombres est {max_number}")
