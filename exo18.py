2f262637eddf27c06792eff104fd5c7e2c4d1a2d
farouck98 <latoundjifarouck@gmail.com>
2023-12-02 17:01:27 +0100

    Exercice 18

---
+++ exo18.py
@@ -0,0 +1,7 @@
+user_char = input("Entrez une chaîne de caractère: ")
+for char in range(len(user_char)):
+    if (user_char[char] == "a"):
+        print(f"La lettre a se trouve à la position {char} dans la chaîne de caractère")  
+
+
+        
