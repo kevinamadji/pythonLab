aed966eb40b81fce6846a26b751885079521f4a4
farouck98 <latoundjifarouck@gmail.com>
2023-12-02 13:33:52 +0100

    Exercice 10

---
+++ exo10.py
@@ -0,0 +1,11 @@
+from math import pi
+circle_radius = float(input("Entrez le rayon du cercle: "))
+"""
+surface = 3.14 * circle_radius**2
+perimetre = 2*3.14*circle_radius
+
+"""
+surface = pi*circle_radius**2
+perimetre = 2*pi*circle_radius
+
+print(surface, perimetre)
